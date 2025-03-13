7d5cdaa953e094abcee11e1739f7419478dff324
GoNEAR <cagatay.morgul@outlook.com>
2025-03-13 14:13:21 +0100

    ajout dune ombre a chaque click de card

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,6 @@
-let toutLesCards = document.querySelectorAll('.cards')
+let toutLesCards = document.querySelectorAll('.card')
+toutLesCards.forEach(card => {
+    card.addEventListener('click', () => {
+        card.style.boxShadow = '5px 5px 10px gray'
+    })
+})
