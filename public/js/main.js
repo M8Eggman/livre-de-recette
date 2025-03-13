bbcc3c5276117c97086af60f9d3799cfd07340f8
GoNEAR <cagatay.morgul@outlook.com>
2025-03-13 14:20:45 +0100

    ajout dune couleur gris quon en passe sur une card

--- public/js/main.js
+++ public/js/main.js
@@ -3,4 +3,7 @@ toutLesCards.forEach(card => {
     card.addEventListener('click', () => {
         card.style.boxShadow = '5px 5px 10px gray'
     })
+    card.addEventListener('mouseover', () => {
+        card.style.backgroundColor = 'gray'
+    })
 })
