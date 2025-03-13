8fd2a92c1cd769a19e5ea45c32b8a2e8fd6fe8b6
GoNEAR <cagatay.morgul@outlook.com>
2025-03-13 14:35:36 +0100

    ajout de lien grace a js pour la premiere section

--- public/js/main.js
+++ public/js/main.js
@@ -10,3 +10,15 @@ toutLesCards.forEach(card => {
         card.style.backgroundColor ='white'
     })
 })
+let section1 = document.querySelectorAll('section')[1]
+let boutonDePlat = section1.querySelectorAll('.btn')
+console.log(boutonDePlat)
+boutonDePlat[0].addEventListener('click', () => {
+    window.location.href="./public/html/Chili con carne Vénézuelien.html"
+})
+boutonDePlat[1].addEventListener('click', () => {
+    window.location.href="./public/html/Civet de Sanglier à la bourguignonne.html"
+})
+boutonDePlat[2].addEventListener('click', () => {
+    window.location.href="./public/html/Tartiflette.html"
+})
